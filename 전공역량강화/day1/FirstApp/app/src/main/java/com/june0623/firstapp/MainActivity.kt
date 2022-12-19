6e39cc27f4b49cae8310ebb59c4422fa4ea1c033
username <81845513+june3011@users.noreply.github.com>
2022-12-19 22:57:55 +0900

    Study : first android
    기본 설정(안드로이드 스튜디오 설치, 에뮬레이터 설치), 간단한 textView 수정하기

---
+++ 전공역량강화/day1/FirstApp/app/src/main/java/com/june0623/firstapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.june0623.firstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView:TextView = findViewById(R.id.txtView)
+        textView.setText("Hello DGSW!")
+    }
+}
